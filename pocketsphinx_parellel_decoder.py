9a1333e2abea586d0976d879f0f52c83cc80a5e5
Motaz Saad <motaz.saad@gmail.com>
2018-08-01 13:38:01 +0300

    process n files at a time in parallel (a stable version)

--- pocketsphinx_parellel_decoder.py
+++ pocketsphinx_parellel_decoder.py
@@ -11,10 +11,12 @@ pip install --upgrade pocketsphinx
 import argparse
 import datetime
 import glob
+import multiprocessing
 import os
 import sys
 import time
 import configparser
+from multiprocessing.pool import Pool
 
 from pocketsphinx import DefaultConfig, Decoder, get_model_path
 from pydub import AudioSegment
@@ -39,22 +41,39 @@ def load_decoder(model_config):
     return decoder_engine
 
 
-def decode_wave(wave, wav_decoder):
-    wav_decoder.start_utt()
-    stream = wave
+def decode_wave(wav_file):
+    decode_result = {}
+    audio_segment = AudioSegment.from_file(wav_file, format=args.format)
+    duration = audio_segment.duration_seconds
+    wav_stream = open(wav_file, 'rb')
+    my_decoder.start_utt()
     while True:
-        buf = stream.read(1024)
+        buf = wav_stream.read(1024)
         if buf:
-            wav_decoder.process_raw(buf, False, False)
+            my_decoder.process_raw(buf, False, False)
         else:
             break
-    wav_decoder.end_utt()
-    hypothesis = wav_decoder.hyp()
+    my_decoder.end_utt()
+    hypothesis = my_decoder.hyp()
     try:
-        message = hypothesis.hypstr
+        text = hypothesis.hypstr
     except AttributeError:
-        message = ''
-    return message
+        text = ''
+    decode_result[wav_file] = (duration, text)
+    return decode_result
+
+
+def split_list(data, n_parts):
+    if len(data) <= n_parts:
+        return [data]
+    else:
+        part_size = int(len(data) / n_parts)
+        # print('part size {}'.format(part_size))
+        parts = [data[x:x + part_size] for x in range(0, len(data), part_size)]
+        if len(parts[-1]) < part_size:
+            parts[-2].extend(parts[-1])
+            del parts[-1]
+        return parts
 
 
 parser = argparse.ArgumentParser(description='This decoder is based CMU Sphinx (works offline) engine provided by '
@@ -68,37 +87,53 @@ parser.add_argument('-f', '--format', type=str,
 if __name__ == '__main__':
     cpu_count = os.cpu_count()
     print('number of CPUs: {}'.format(cpu_count))
-    config = configparser.ConfigParser()
-    start_time = time.time()
     args = parser.parse_args()
     wav_dir = args.indir
-    audio_format = args.format
-    if audio_format != 'wav':
-        print('The current implementation does not support {}'.format(audio_format))
+    if args.format != 'wav':
+        print('The current implementation does not support {}'.format(args.format))
         sys.exit(-1)
-    audio_files = glob.glob(os.path.join(wav_dir, '*.{}'.format(audio_format)))
+    audio_files = glob.glob(os.path.join(wav_dir, '*.{}'.format(args.format)))
     num_files = len(audio_files)
     if num_files == 0:
-        print("no {} found in {}".format(audio_format, wav_dir))
+        print("no {} found in {}".format(args.format, wav_dir))
         sys.exit(-1)
-    print('# of audio files: {}'.format(len(audio_files)))
+    print('# of audio files: {}'.format(num_files))
+    wav_file_lists = split_list(audio_files, cpu_count)
+    print('number of parts: {}'.format(len(wav_file_lists)))
+    for i, p in enumerate(wav_file_lists):
+        print('part {} has {} audio segments'.format(i, len(p)))
+    ###################################################
+    # load configuration
+    config = configparser.ConfigParser()
     conf_file = args.conf
     if not os.path.exists(conf_file):
         print('{} doest not exisit'.format(conf_file))
     config.read(conf_file)
     print('loading decoder models ...')
-    decoder = load_decoder(config)
+    my_decoder = load_decoder(config)
+    print('decoder has been loaded ...')
     total_duration = 0
-    for audio_file in audio_files:
-        print('processing', audio_file)
-        audio_segment = AudioSegment.from_file(audio_file, format=audio_format)
-        total_duration += audio_segment.duration_seconds
-        # print('wave duration: {}'.format(datetime.timedelta(seconds=audio_segment.duration_seconds)))
-        # print('loading wave stream ...')
-        wav_stream = open(audio_file, 'rb')
-        # print('decoding ...')
-        result = decode_wave(wav_stream, decoder)
-        print("CMU Sphinx transcribed the file as: \n{}\n".format(result))
+    ###################################################
+    start_time = time.time()
+    # process lists:
+    print('processing lists')
+    results = {}
+    for i, wav_list in enumerate(wav_file_lists):
+        print('process {} files in parallel in part {}'.format(len(wav_list), i))
+        pool = multiprocessing.Pool(processes=cpu_count)
+        result = pool.map(decode_wave, wav_list)
+        #print(result)
+        for r in result:
+            results.update(r)
+    ##########################################
+    for k, v in results.items():
+        print('file: {}'.format(k))
+        file_duration, transcription = v
+        print('duration: {}'.format(file_duration))
+        print('transcription: \n{}'.format(transcription))
+        print('######################################')
+        total_duration += file_duration
+
     print('total audio duration: {}'.format(datetime.timedelta(seconds=total_duration)))
     decode_time = time.time() - start_time
     decode_time_str = "decode time: {}".format(datetime.timedelta(seconds=decode_time))
