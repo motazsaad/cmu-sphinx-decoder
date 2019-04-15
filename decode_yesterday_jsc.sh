cd450b735fafdc667bd5832ed5274802a32d02ba
Motaz Saad <motaz.saad@gmail.com>
2019-04-15 06:31:06 +0300

    fix log path

--- decode_yesterday_jsc.sh
+++ decode_yesterday_jsc.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+channel="2713"
+
 yesterday=$(date -d "yesterday" '+%Y/%m/%d')
 #echo "yesterday"
 #echo ${yesterday}
 yesterday_log=$(date -d "yesterday" '+%Y_%m_%d')
 
-nohup python3 /home/sphinxuser/cmu-sphinx-decoder/pocketsphinx_parallel_decoder.py -j 8 -c /home/sphinxuser/cmu-sphinx-decoder/conf/config_ar.ini -i /storage/recordings/2713/${yesterday}  -o decoder_out/ > /home/sphinxuser/logs/${yesterday_log}.log 2>&1
+nohup python3 /home/sphinxuser/cmu-sphinx-decoder/pocketsphinx_parallel_decoder.py -j 8 -c /home/sphinxuser/cmu-sphinx-decoder/conf/config_ar.ini -i /storage/recordings/${channel}/${yesterday}  -o decoder_out/ > /home/sphinxuser/logs/${channel}_${yesterday_log}.log 2>&1
